b09a95465d9f05f6d7c181aebe07e5c0f35b551e
Bhendon Soares <bhendon.soares12@gmail.com>
2024-02-20 16:57:08 -0300

    refactor: transforma a lista de pokemon em uma lista html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,3 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-
 function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemon">
@@ -21,9 +17,6 @@ function convertPokemonToLi(pokemon) {
 
 const pokemonList = document.getElementById('pokemonList');
 
-pokeApi.getPokemons().then((pokemons) => {
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
-            pokemonList.innerHTML += convertPokemonToLi(pokemon);
-        }
+pokeApi.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('');
 })
